32b609d6b0e116d44519362a564244d027b2a3fc
paulampkelly <paula.kelly@dit.ie>
2023-04-23 20:50:54 +0100

    Update loginForm.php

--- loginForm.php
+++ loginForm.php
@@ -15,6 +15,12 @@
             <p><input type = "submit" name = "submit" value = "login"></p>
         </form>
 
+         <form name ="form1" method = "post" action = "checkLogin.php"> <!--lab6: Part1, Point 8 -->
+            <p>Username <input type = "text" name = "username" value = "" maxlength="20"/></p>
+            <p>ID <input type = "text" name = "ID" value = "" maxlength="4"/></p>
+            <p><input type = "submit" name = "submit" value = "login"></p>
+        </form>
+
             <?PHP
                 if (isset($_SESSION['error'])) { // if there is an errorMessage session variable set
                    print($_SESSION['error']); //print the errorMessage session variable value
